d020214574be1bcecc0c2d9f47e996d0dc4b73f8
Antoine <antoinedery12@hotmail.fr>
2021-12-30 12:13:13 -0500

    homepage done

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AppComponent } from './pages/app/app.component';
-import { HomeSearchBarComponent } from './components/home-search-bar/home-search-bar.component';
+import { HomeSearchBarComponent } from './components/home-page-folder/home-search-bar/home-search-bar.component';
 import { LyricsDisplayComponent } from './components/lyrics-display/lyrics-display.component';
+import { FooterComponent } from './components/home-page-folder/footer/footer.component';
+import { AppRoutingModule } from './modules/app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, HomeSearchBarComponent, LyricsDisplayComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    HomeSearchBarComponent,
+    LyricsDisplayComponent,
+    FooterComponent,
+  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
